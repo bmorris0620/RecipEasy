015333997974e39230894c1c595d6603941c8cb4
Brittany Morris <bmorris03@aurora.edu>
2016-09-26 15:41:57 -0500

    Created routes & views, edited controller
    Created the routes and views for login, registration, and home page.
    Also created Pages controller with methods for each page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  get 'pages/login'
+
+  get 'pages/register'
+
+  get 'pages/home'
+
+	get 'login' => 'pages#login'
+
+	get 'register' => 'pages#register'
+
+	get 'home' => 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
